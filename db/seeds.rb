2b337ac8063aaf8599aa436598f50f3b8a99b665
Mera-Stackhouse <mera.stackhouse@gmail.com>
2019-07-19 00:28:22 -0700

    updates seeds and starts readme

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,14 @@
 
 User.destroy_all
-Pet.destroy_all
-# Vet.destroy_all
+Pet.destroy_all 
 
-
+puts 'Seeding Users...'
 mera = User.create(username: "Mera", password: "hello", avatar_url: 'https://vignette.wikia.nocookie.net/dccu/images/9/97/Aquaman_Princess_Mera_Character_Textless_Poster.jpg/revision/latest/scale-to-width-down/2000?cb=20181123170545', mobile: '206-555-1234', email: "mera@ilovepets.com", neighborhood: "Shoreline")
 
 bob = User.create(username: "Bob", password: "bye", avatar_url: 'https://vignette.wikia.nocookie.net/dccu/images/9/97/Aquaman_Princess_Mera_Character_Textless_Poster.jpg/revision/latest/scale-to-width-down/2000?cb=20181123170545', mobile: '206-555-1234', email: "mera@ilovepets.com", neighborhood: "Shoreline")
 
+
+puts 'Seeding Pets...'
 meatball = Pet.create(name: 'Meatball', species: 'Cat', breed: 'Domestic Shorthair', birthdate: Time.new(2012, 12, 30), gender: 'female', img_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/14_years_old_american_shorthair.jpg/220px-14_years_old_american_shorthair.jpg')
 
 ben = Pet.create(name: 'Ben', species: 'Dog', breed: 'Miniature Dachshund', birthdate: Time.new(2012, 12, 30), gender: 'male', img_url: 'https://cdn.shopify.com/s/files/1/0994/0236/files/miniature-dachshund_large.jpg?v=1505754817')
@@ -17,6 +18,7 @@ abbey = Pet.create(name: 'Abbey', species: 'Cat', breed: 'Domestic Shorthair - C
 shadow = Pet.create(name: 'Shadow', species: 'Cat', breed: 'Domestic Shorthair', birthdate: Time.new(2012, 12, 30), gender: 'female', img_url: 'https://media.angieslist.com/s3fs-public/styles/widescreen_large/public/styrofoam-cat-shelter.jpg?itok=BCQlgbUD')
 
 
+puts 'Seeding Relationships...'
 PetsUser.create(user: mera , pet: meatball)
 PetsUser.create(user: mera , pet: ben)
 PetsUser.create(user: mera , pet: abbey)
